ba3ac8478834aaad2c6c3d32471983d0718f7771
declau <dec82@hotmail.com>
2016-05-27 19:47:57 -0300

    Strategy

---
+++ strategy/src/strategy/CalculadorDeImpostos.java
@@ -0,0 +1,13 @@
+package strategy;
+
+public class CalculadorDeImpostos {
+	
+	public void realizaCalculo(Orcamento orcamento, Imposto impostoQualquer){
+		
+		double icms = impostoQualquer.calcula(orcamento);
+			System.out.println(icms);	
+	
+	}
+
+}
+	
